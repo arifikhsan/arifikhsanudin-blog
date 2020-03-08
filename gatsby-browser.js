64f27820d32a99c583c5d8802eef81c6faf2f1a5
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-03-08 15:21:44 +0700

    ga

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,4 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-// import "prismjs/themes/prism.css"
-// import "prismjs/themes/prism-okaidia.css"
-// import "prism-themes/themes/prism-vsc-dark-plus.css"
-// import "prism-themes/themes/prism-material-oceanic.css"
-// import "prism-themes/themes/prism-ghcolors.css"
 import "prism-themes/themes/prism-dracula.css"
