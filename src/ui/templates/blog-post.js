fdf3481f7355d9685012d051717f0aaf3e404e9b
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-06-25 21:46:43 +0700

    up

--- src/ui/templates/blog-post.js
+++ src/ui/templates/blog-post.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import SEO from "../components/seo"
