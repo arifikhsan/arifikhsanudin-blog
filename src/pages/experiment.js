cec2cf0b2ab55df89ef7a264927af9c67cdfcaab
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-06 17:17:47 +0700

    ex

--- src/pages/experiment.js
+++ src/pages/experiment.js
@@ -1,45 +1,45 @@
-// import React from "react"
-// import { graphql } from "gatsby"
-// import ExperimentScreen from "../ui/screens/experiment/ExperimentScreen"
+import React from "react"
+import { graphql } from "gatsby"
+import ExperimentScreen from "../ui/screens/experiment/ExperimentScreen"
 
-// const ExperimentPage = ({ data, location, pageContext: { locale } }) => {
-//   return <ExperimentScreen data={data} location={location} locale={locale} />
-// }
+const ExperimentPage = ({ data, location, pageContext: { locale } }) => {
+  return <ExperimentScreen data={data} location={location} locale={locale} />
+}
 
-// export default ExperimentPage
+export default ExperimentPage
 
-// export const pageQuery = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     experiments: allFile(
-//       filter: {
-//         sourceInstanceName: { eq: "experiment" }
-//         extension: { eq: "md" }
-//       }
-//       sort: { fields: [childMarkdownRemark___frontmatter___date], order: DESC}
-//       limit: 1000
-//     ) {
-//       edges {
-//         node {
-//           # childMarkdownRemark {
-//           #   id
-//           #   fields {
-//           #     slug
-//           #   }
-//             frontmatter {
-//               title
-//               description
-//               github_url
-//             }
-//           # }
-//           extension
-//           name
-//         }
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    experiments: allFile(
+      filter: {
+        sourceInstanceName: { eq: "experiment" }
+        extension: { eq: "md" }
+      }
+      sort: { fields: [childMarkdownRemark___frontmatter___date], order: DESC}
+      limit: 1000
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              description
+              github_url
+            }
+          }
+          extension
+          name
+        }
+      }
+    }
+  }
+`
